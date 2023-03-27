a60cd6d34456e81f2bd79aaeaca746cb9fff968a
Aj Narag <ajnarag25@gmail.com>
2023-03-27 20:50:17 +0800

    change the starting files and downgraded the current version of .net

--- Pages/Error.cshtml.cs
+++ Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace aspnetcorewebapp.Pages;
+namespace Crud_Web_App.Pages;
 
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [IgnoreAntiforgeryToken]
--- Pages/Privacy.cshtml.cs
+++ Pages/Privacy.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace aspnetcorewebapp.Pages;
+namespace Crud_Web_App.Pages;
 
 public class PrivacyModel : PageModel
 {
